e5fa681459066d873eba160f0db45885312dfae0
Janet Kenmotsu <janet@ditto.com>
2020-10-20 10:35:55 -0700

    add more links for setup

---
+++ zshell_settings/alias.sh
@@ -0,0 +1,9 @@
+# ~/.oh-my-zsh/custom/alias.zsh
+alias seba="source env/bin/activate"
+alias dea="deactivate"
+alias grbm="git rebase -i origin/master"
+alias pipi="pip install"
+alias vh="vagrant halt"
+alias vu="vagrant up"
+alias vs="vagrant suspend"
+alias vsh="vagrant ssh"
